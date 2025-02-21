654a911d04c80cb81581f2eccabb3aab9366575b
yuvcarl <carldrake969@gmail.com>
2025-02-21 21:13:21 +0100

    updated the sqlfnction file

--- utils/sqlFunctions.js
+++ utils/sqlFunctions.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const config = require("../auth/db/config");
+const config = require("../db/config");
 const pool = mysql.createPool(config);
 
 const createTable = (schema) => {
